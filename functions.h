72a30acee5ab2014a345f497b244900b88224a52
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-06-01 14:21:47 +0200

    Update functions.h

--- functions.h
+++ functions.h
@@ -5,5 +5,6 @@ void copy_file(const char *input_file, const char *output_file, int block_size, 
 void create_iso(const char *source_dir, const char *output_file);
 void burn_cd_dvd(const char *device, const char *image);
 void burn_dmg(const char *image_file, const char *device);
+void create_app_image(const char *source_dir, const char *output_file, const char *volume_name, int size_mb);
 
 #endif // FUNCTIONS_H
