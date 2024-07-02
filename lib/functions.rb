b7c3feadb7ef8d694ba21545be53b3994ec2288f
mark1n0 <127632468+mgxy0@users.noreply.github.com>
2024-07-02 14:27:09 +0200

    Update functions.rb

--- lib/functions.rb
+++ lib/functions.rb
@@ -1,55 +1,70 @@
 require 'fileutils'
 require 'open3'
 
+def execute_command_for_os(command_macos, command_linux)
+  if RUBY_PLATFORM =~ /darwin/
+    system(command_macos)
+  elsif RUBY_PLATFORM =~ /linux/
+    system(command_linux)
+  else
+    puts "Sistema operativo non supportato."
+  end
+end
+
 def copy_file(input_file, output_file)
   unless File.exist?(input_file)
-    raise "Input file does not exist."
+    raise "Il file di input non esiste: #{input_file}"
   end
   FileUtils.cp(input_file, output_file)
+  puts "File copiato con successo da #{input_file} a #{output_file}"
 rescue => e
-  raise "Error copying file: #{e.message}"
+  raise "Errore durante la copia del file: #{e.message}"
 end
 
 def create_iso(source_dir, output_file)
   unless Dir.exist?(source_dir)
-    raise "Source directory does not exist."
+    raise "La directory di origine non esiste: #{source_dir}"
   end
-  cmd = ["hdiutil", "makehybrid", "-o", output_file, source_dir, "-iso", "-joliet"]
-  stdout, stderr, status = Open3.capture3(*cmd)
-  raise "Error creating ISO: #{stderr}" unless status.success?
+  cmd_macos = ["hdiutil", "makehybrid", "-o", output_file, source_dir, "-iso", "-joliet"]
+  cmd_linux = ["genisoimage", "-o", output_file, source_dir]
+  execute_command_for_os(cmd_macos.join(" "), cmd_linux.join(" "))
+  puts "ISO creato con successo: #{output_file}"
 rescue => e
-  raise "Error creating ISO: #{e.message}"
+  raise "Errore durante la creazione dell'ISO: #{e.message}"
 end
 
 def burn_cd_dvd(device, image_file)
   unless File.exist?(image_file)
-    raise "Image file does not exist."
+    raise "Il file immagine non esiste: #{image_file}"
   end
-  cmd = ["hdiutil", "burn", image_file, "-device", device]
-  stdout, stderr, status = Open3.capture3(*cmd)
-  raise "Error burning CD/DVD: #{stderr}" unless status.success?
+  cmd_macos = ["hdiutil", "burn", image_file, "-device", device]
+  cmd_linux = ["wodim", "-v", "dev=#{device}", "speed=4", "-eject", "fs=8m", "padsize=63s", image_file]
+  execute_command_for_os(cmd_macos.join(" "), cmd_linux.join(" "))
+  puts "CD/DVD masterizzato con successo su #{device}"
 rescue => e
-  raise "Error burning CD/DVD: #{e.message}"
+  raise "Errore durante la masterizzazione del CD/DVD: #{e.message}"
 end
 
 def burn_dmg(image_file, device)
   unless File.exist?(image_file)
-    raise "Image file does not exist."
+    raise "Il file immagine DMG non esiste: #{image_file}"
   end
-  cmd = ["sudo", "dd", "if=#{image_file}", "of=#{device}", "bs=1m"]
-  stdout, stderr, status = Open3.capture3(*cmd)
-  raise "Error burning DMG: #{stderr}" unless status.success?
+  cmd_macos = ["sudo", "dd", "if=#{image_file}", "of=#{device}", "bs=1m"]
+  cmd_linux = ["sudo", "dd", "if=#{image_file}", "of=#{device}", "bs=1M", "status=progress"]
+  execute_command_for_os(cmd_macos.join(" "), cmd_linux.join(" "))
+  puts "DMG masterizzato con successo su #{device}"
 rescue => e
-  raise "Error burning DMG: #{e.message}"
+  raise "Errore durante la masterizzazione del DMG: #{e.message}"
 end
 
 def create_install_media(installer_path, volume)
   unless File.exist?(installer_path)
-    raise "Installer path does not exist."
+    raise "Il percorso dell'installer non esiste: #{installer_path}"
   end
-  cmd = ["sudo", installer_path, "--volume", volume, "--nointeraction"]
-  stdout, stderr, status = Open3.capture3(*cmd)
-  raise "Error creating install media: #{stderr}" unless status.success?
+  cmd_macos = ["sudo", installer_path, "--volume", volume, "--nointeraction"]
+  cmd_linux = ["echo", "Linux non supporta questa funzione di installazione"]
+  execute_command_for_os(cmd_macos.join(" "), cmd_linux.join(" "))
+  puts "Media di installazione creato con successo su #{volume}"
 rescue => e
-  raise "Error creating install media: #{e.message}"
+  raise "Errore durante la creazione del media di installazione: #{e.message}"
 end
